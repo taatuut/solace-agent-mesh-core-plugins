76ca6ee3fd443c6bfb09ed3abe106aea87698b64
Emil Zegers <emil.zegers@solace.com>
2025-06-20 12:38:48 +0200

    Testing fix for schema detection.

--- sam-graph-database/src/agents/graph_database/graph_database_agent_component.py
+++ sam-graph-database/src/agents/graph_database/graph_database_agent_component.py
@@ -327,8 +327,8 @@ class GraphDatabaseAgentComponent(BaseAgentComponent):
 
         def get_schema(tx, *args, **kwargs):
             result = tx.run("CALL db.schema.visualization()")
-            serialized_schema = serialize_neo4j_schema(result)
-            serialized_schema = dict(serialized_schema.single())
+            serialized_schema = dict(serialize_neo4j_schema(result.single()))
+            #serialized_schema = dict(serialized_schema.single())
             print("bo ez serialized_schema")
             print()
             pprint.pprint(serialized_schema)
